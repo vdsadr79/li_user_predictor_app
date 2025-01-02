0109fcc2e30410ad3a9e580f5dd819ada64235b4
vdsadr79 <vd275@georgetown.edu>
2025-01-02 16:31:42 -0500

    updated interface for income and education levels

--- li_user_predictor.py
+++ li_user_predictor.py
@@ -11,8 +11,35 @@ st.title("LinkedIn User Prediction App")
 st.write("Enter the following details to predict whether you're likely to use LinkedIn and the probability of usage.")
 
 # User inputs for model features
-income = st.number_input("Income (1-9):", min_value=1, max_value=9, step=1)
-education = st.number_input("Education Level (1-8):", min_value=1, max_value=8, step=1)
+income = st.selectbox(
+    "Income:",
+    options=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+    format_func=lambda x: [
+        "Less than $10,000",
+        "$10,000 - $20,000",
+        "$20,000 - $30,000",
+        "$30,000 - $40,000",
+        "$40,000 - $50,000",
+        "$50,000 - $75,000",
+        "$75,000 - $100,000",
+        "$100,000 - $150,000",
+        "More than $150,000"
+    ][x - 1]
+)
+education = st.selectbox(
+    "Education Level:",
+    options=[1, 2, 3, 4, 5, 6, 7, 8],
+    format_func=lambda x: [
+        "Less than High School",
+        "High School Graduate",
+        "Some College",
+        "Associate Degree",
+        "Bachelor's Degree",
+        "Master's Degree",
+        "Professional Degree",
+        "Doctorate"
+    ][x - 1]
+)
 parent = st.radio("Are you a parent?", options=[0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
 married = st.radio("Are you married?", options=[0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
 female = st.radio("Are you female?", options=[0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
